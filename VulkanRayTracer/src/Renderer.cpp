557548cc2dc27b5d7ed6e327818292e00ffdae68
SahKh <145230870+SahKh@users.noreply.github.com>
2024-01-16 22:45:10 -0500

    simple shading

--- VulkanRayTracer/src/Renderer.cpp
+++ VulkanRayTracer/src/Renderer.cpp
@@ -2,6 +2,20 @@
 
 #include "Walnut/Random.h"
 
+#include <cmath>
+
+namespace Utilities {
+	static uint32_t ConvertVec4ToRGBA(const glm::vec4& color) {
+		uint8_t r = (uint8_t)(color.r * 255.0f);
+		uint8_t g = (uint8_t)(color.g * 255.0f);
+		uint8_t b = (uint8_t)(color.b * 255.0f);
+		uint8_t a = (uint8_t)(color.a * 255.0f);
+
+		//bitwise operations to shift values into correct place in memory 
+		uint32_t result = (a << 24) | (b << 16) | (g << 8) | r;
+		return result;
+	}
+}
 void Renderer::OnResize(uint32_t width, uint32_t height) {
 
 	if (m_FinalImage) {
@@ -22,14 +36,16 @@ void Renderer::Render() {
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
 			coord = coord * 2.0f - 1.0f;
-			m_ImageData[x + y * m_FinalImage->GetWidth()] = PerPixel(coord);
+			glm::vec4 color = PerPixel(coord);
+			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
+			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utilities::ConvertVec4ToRGBA(color);
 		}
 	}
 	m_FinalImage->SetData(m_ImageData);
 }
 
-uint32_t Renderer::PerPixel(glm::vec2 coord) {
-	glm::vec3 rayOrigin(0.0f, 0.0f, -2.0f);
+glm::vec4 Renderer::PerPixel(glm::vec2 coord) {
+	glm::vec3 rayOrigin(0.0f, 0.0f, -1.0f);
 	glm::vec3 rayDirection(coord.x, coord.y, -1.0f);
 	float radius = 0.5f;
 
@@ -51,10 +67,27 @@ uint32_t Renderer::PerPixel(glm::vec2 coord) {
 	//Discriminant
 	float discriminant = b * b - (4.0f * a * c);
 
-	if (discriminant >= 0.0f) { return 0xffff00ff; }
+	if (discriminant < 0.0f) {
+		return glm::vec4(0, 0, 0, 1);
+	}
 
-	return 0xff000000;
+	float x0 = (-b + glm::sqrt(discriminant) / 2.0f * a); //ray on way out
+	float x1 = (-b - glm::sqrt(discriminant) / 2.0f * a);
+
+	glm::vec3 hitPosition = rayOrigin + rayDirection * x1;
+	glm::vec3 normal = glm::normalize(hitPosition);
+	glm::vec3 sphereColor(1, 0, 1);
+	sphereColor = normal;
+	return glm::vec4(sphereColor, 1.0f);
 }
 
 // no scaling, need to account for viewport width and height when accounting for -1 to 1 range
 
+/*
+
+		{
+			
+		}
+		
+		return 0xffff00ff; }
+		*/
--- VulkanRayTracer/src/Renderer.h
+++ VulkanRayTracer/src/Renderer.h
@@ -16,7 +16,7 @@ private:
 	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
 
-	uint32_t PerPixel(glm::vec2 coord);
+	glm::vec4 PerPixel(glm::vec2 coord);
 
 
 };
