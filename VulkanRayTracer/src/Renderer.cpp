0b525c08d24c1aa2580277b26d751bc76377c56c
SahKh <145230870+SahKh@users.noreply.github.com>
2024-01-16 23:06:00 -0500

    added intensity to shading

--- VulkanRayTracer/src/Renderer.cpp
+++ VulkanRayTracer/src/Renderer.cpp
@@ -35,7 +35,7 @@ void Renderer::Render() {
 	for (uint32_t y = 0; y < m_FinalImage->GetHeight(); y++) {
 		for (uint32_t x = 0; x < m_FinalImage->GetWidth(); x++) {
 			glm::vec2 coord = { (float)x / (float)m_FinalImage->GetWidth(), (float)y / (float)m_FinalImage->GetHeight() };
-			coord = coord * 2.0f - 1.0f;
+			coord = coord * -2.0f + 1.0f;
 			glm::vec4 color = PerPixel(coord);
 			color = glm::clamp(color, glm::vec4(0.0f), glm::vec4(1.0f));
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = Utilities::ConvertVec4ToRGBA(color);
@@ -59,7 +59,6 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord) {
 	//calculate ray intersection with object using quadratic formula
 	// if there is a solution/intersection then the ray hit
 	
-
 	float a = glm::dot(rayDirection, rayDirection);
 	float b = 2.0f * glm::dot(rayOrigin, rayDirection);
 	float c = glm::dot(rayOrigin, rayOrigin) - radius * radius;
@@ -76,18 +75,15 @@ glm::vec4 Renderer::PerPixel(glm::vec2 coord) {
 
 	glm::vec3 hitPosition = rayOrigin + rayDirection * x1;
 	glm::vec3 normal = glm::normalize(hitPosition);
+	glm::vec3 lightDirection = glm::normalize(glm::vec3(-1, -1, -1));
+
+	float intensity = glm::max(glm::dot(normal, -lightDirection), 0.0f);
+
 	glm::vec3 sphereColor(1, 0, 1);
-	sphereColor = normal;
+	sphereColor = normal * 0.5f + 0.5f;
+	sphereColor *= intensity;
 	return glm::vec4(sphereColor, 1.0f);
 }
 
 // no scaling, need to account for viewport width and height when accounting for -1 to 1 range
 
-/*
-
-		{
-			
-		}
-		
-		return 0xffff00ff; }
-		*/
